15eb9ab388cbb7538feb073f0f5d13a6568fd62c
VasilLozev <108930879+VasilLozev@users.noreply.github.com>
2022-11-06 20:57:50 +0200

    Add files via upload

---
+++ 3. Primary Diagonal/3. Primary Diagonal/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Diagnostics.CodeAnalysis;
+
+namespace _3._Primary_Diagonal
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            int [,] matrix = new int[n, n];
+            int sum = 0;
+            for (int i = 0; i < n; i++)
+            {
+                string[] strings = Console.ReadLine().Split(' ');
+                for (int j = 0; j < n; j++)
+                {
+                    matrix[i, j] = int.Parse(strings[j]);
+                    if (i == j)
+                    {
+                        sum += matrix[i, j];
+                    }
+                }
+            }
+            Console.WriteLine(sum);
+        }
+    }
+}
