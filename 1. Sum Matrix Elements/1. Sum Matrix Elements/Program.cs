dd4c15ee13f31ccb70691eb53547f237f6302b93
VasilLozev <108930879+VasilLozev@users.noreply.github.com>
2022-11-06 15:31:56 +0200

    Add files via upload

---
+++ 1. Sum Matrix Elements/1. Sum Matrix Elements/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace _1._Sum_Matrix_Elements
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] counts = Console.ReadLine().Split(", ");
+            int rows = int.Parse(counts[0]);
+            int cols = int.Parse(counts[1]);
+            int[,] matrix = new int[rows, cols];
+
+                for (int i = 0; i < rows; i++)
+                {
+                string[] x = Console.ReadLine().Split(", ");
+                    for (int j = 0; j < cols; j++)
+                    {
+                    matrix[i, j] = int.Parse(x[j]);
+                    }
+                }
+           
+            Console.WriteLine(rows);
+            Console.WriteLine(cols);
+
+            int count = 0;
+            foreach (var item in matrix)
+            {
+                count += item;
+            }
+            Console.WriteLine(count);
+        }
+    }
+}
