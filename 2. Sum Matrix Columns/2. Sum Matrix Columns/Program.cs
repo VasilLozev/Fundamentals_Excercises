1800092a15fc94b59ad75ab294515fc77947e4be
VasilLozev <108930879+VasilLozev@users.noreply.github.com>
2022-11-06 20:38:52 +0200

    Add files via upload

---
+++ 2. Sum Matrix Columns/2. Sum Matrix Columns/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace _2._Sum_Matrix_Columns
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] rowCol = Console.ReadLine().Split(", ");
+            int rows = int.Parse(rowCol[0]);
+            int cols = int.Parse(rowCol[1]);
+            int[,] matrix = new int[rows, cols];
+            for (int i = 0; i < rows; i++)
+            {
+                string[] strings = Console.ReadLine().Split(" ");
+                for (int j = 0; j < cols; j++)
+                {
+                    matrix[i, j] = int.Parse(strings[j]);
+                }
+            }
+            for (int j = 0; j < cols; j++)
+            {
+                int sum = 0;
+                for (int i = 0; i < rows; i++)
+                {
+                    sum += matrix[i, j];
+                }
+                Console.WriteLine(sum);
+            }
+        }
+    }
+}
