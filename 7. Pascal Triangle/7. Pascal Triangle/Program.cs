a79da6734bb0d05a50e824a101314ff4ab015784
VasilLozev <108930879+VasilLozev@users.noreply.github.com>
2022-11-09 01:35:36 +0200

    Add files via upload

---
+++ 7. Pascal Triangle/7. Pascal Triangle/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace _7._Pascal_Triangle
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int number = int.Parse(Console.ReadLine());
+            int[,] numbers = new int[number, number];
+            numbers[0, 0] = 1;
+            Console.Write(1);
+            for (int i = 1; i < number; i++)
+            {
+                for (int j = 0; j <= i; j++)
+                {
+                    if (j == 0)
+                    {
+                        numbers[i, j] = 1;
+                        Console.Write("\n" + numbers[i, j] + " ");
+                    }
+                    else if (j == i)
+                    {
+                        numbers[i, j] = 1;
+                        Console.Write(numbers[i, j] + " ");
+                    }
+                    else
+                    {
+                        numbers[i, j] = numbers[i - 1, j - 1] + numbers[i - 1, j];
+                        Console.Write(numbers[i, j] + " ");
+                    }
+                }
+            }
+        }
+    }
+}
