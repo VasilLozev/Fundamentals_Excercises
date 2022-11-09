d92186d4832e8101f6139d3ff3b922515fa89ed0
VasilLozev <108930879+VasilLozev@users.noreply.github.com>
2022-11-09 23:40:39 +0200

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Stack<char> brackets = new Stack<char>();
+            string input = Console.ReadLine();
+            try
+            {
+                foreach (var item in input)
+                {
+                    switch (item)
+                    {
+                        case '(':
+                        case '[':
+                        case '{':
+                            brackets.Push(item);
+                            break;
+                        case ')':
+                            if (brackets.Pop() != '(')
+                            {
+                                Console.WriteLine("NO");
+                                return
+                                ;
+                            }
+                            break;
+                        case ']':
+                            if (brackets.Pop() != '[')
+                            {
+                                Console.WriteLine("NO");
+                                return
+                                ;
+                            }
+                            break;
+                        case '}':
+                            if (brackets.Pop() != '{')
+                            {
+                                Console.WriteLine("NO");
+                                return
+                                ;
+                            }
+                            break;
+                    }
+                }
+            }
+            catch  (System.Exception ex)
+            {
+                Console.WriteLine("NO" );
+                return;
+            }
+
+            if (brackets.Count > 0)
+            {
+                Console.WriteLine("NO");
+            }
+            else
+                Console.WriteLine("YES");
+        }
+    }
+}
