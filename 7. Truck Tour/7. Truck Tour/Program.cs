547b943390d05356d77f3dbc4a774cfa9f88ae50
VasilLozev <108930879+VasilLozev@users.noreply.github.com>
2022-11-11 22:13:27 +0200

    Add files via upload

---
+++ 7. Truck Tour/7. Truck Tour/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Diagnostics.Tracing;
+using System.Linq;
+using System.Transactions;
+
+namespace _7._Truck_Tour
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int N = int.Parse(Console.ReadLine());
+            int[] stationLiters = new int[N];
+            int[] stationDistances = new int[N];
+            for (int i = 0; i <= N - 1; i++)
+            {
+                string[] pairs = Console.ReadLine().Split(' ');
+                stationLiters[i] = int.Parse(pairs[0]);
+                stationDistances[i] = int.Parse(pairs[1]);
+            }
+            for (int i = 0; i < N; i++)
+            {
+                int currentFuel = 0;
+                bool isPossible = true;
+                for (int j = i; j < N; j++)
+                {
+                    currentFuel += stationLiters[j];
+                    currentFuel -= stationDistances[j];
+                    if (currentFuel < 0)
+                    {
+                        isPossible = false;
+                        break;
+                    }
+                }
+                if (isPossible)
+                {
+                    Console.WriteLine(i);
+                    return;
+                }
+            }
+        }
+    }
+}
