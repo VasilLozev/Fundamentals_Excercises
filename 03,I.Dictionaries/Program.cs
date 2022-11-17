31b6a852d295866f69fb2ad4d34b7e5253c4ddd9
VasilLozev <108930879+VasilLozev@users.noreply.github.com>
2022-11-17 14:32:29 +0200

    Add files via upload

---
+++ 03,I.Dictionaries/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+
+namespace _1._Count_Same_Values_in_Array
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            double[] numbers = Console.ReadLine()
+                             .Split(" ")
+                             .Select(double.Parse)
+                             .ToArray();
+
+            Dictionary<double, int> numberTimes = new Dictionary<double, int>();
+
+            foreach (var number in numbers)
+            {
+                if (!numberTimes.ContainsKey(number))
+                {
+                    numberTimes.Add(number, 0);
+                }
+
+                numberTimes[number]++;
+            }
+            foreach (var item in numberTimes)
+            {
+                Console.WriteLine($"{item.Key} - {item.Value} times");
+            }
+        }
+    }
+}
