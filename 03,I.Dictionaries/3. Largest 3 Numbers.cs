c2dc05978558a8c5673eb24c229f3760e5a8d3e2
VasilLozev <108930879+VasilLozev@users.noreply.github.com>
2022-11-18 20:22:44 +0200

    Update 3. Largest 3 Numbers.cs

--- 03,I.Dictionaries/3. Largest 3 Numbers.cs
+++ 03,I.Dictionaries/3. Largest 3 Numbers.cs
@@ -1,37 +1,23 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Security.Cryptography.X509Certificates;
+using System.Numerics;
 
-namespace _3._Largest_3_Numbers
+namespace _5._Cities_by_Continent_and_Country
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            List<int> numbers =
-                new List<int>(Console.ReadLine()
-                .Split(' ')
-                .Select(int.Parse)
-                .ToList());
+            List<int> numbers = Console.ReadLine().Split()
+                .Select(int.Parse).ToList();
 
-            Dictionary<int, int> counts = new Dictionary<int, int>();
+            numbers = numbers
+                .OrderByDescending(x => x)
+                .Take(3)
+                .ToList();
 
-            for (int i = 0; i < numbers.Count; i++)
-            {
-                counts.Add(i, numbers[i]);
-            }
-            counts = counts.OrderByDescending(x => x.Value).ToDictionary(v => v.Key, v => v.Value);
-            int count1 = 0;
-            foreach(var item in counts)
-            {
-                count1++;
-                Console.Write(item.Value + " ");
-                    if(count1 == 3)
-                {
-                    break;
-                }
-            }
+            Console.WriteLine(String.Join(" ",numbers));
         }
     }
 }
