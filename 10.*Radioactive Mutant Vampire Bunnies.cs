7362faff9dcc33db81de6695f539ce29c4e3fffb
VasilLozev <108930879+VasilLozev@users.noreply.github.com>
2022-11-20 00:02:15 +0200

    Update 10.*Radioactive Mutant Vampire Bunnies.cs

--- 10.*Radioactive Mutant Vampire Bunnies.cs
+++ 10.*Radioactive Mutant Vampire Bunnies.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Data;
 using System.Drawing;
@@ -6,113 +6,114 @@ using System.Linq;
 
 namespace _10._Radioactive_Mutant_Vampire_Bunnies
 {
-    internal class Program
+    public class Program
     {
+        static char[][] matrix;
+        static int size_x = 0;
+        static int size_y = 0;
+
+        static int player_x = 0;
+        static int player_y = 0;
+
         static void Main(string[] args)
         {
             int[] size = Console.ReadLine()
                          .Split(" ")
                          .Select(n => int.Parse(n))
                          .ToArray();
-            char[][] matrix = new char[size[0]][];
-            for (int row = 0; row < size[0]; row++)
+
+            size_x = size[0];
+            size_y = size[1];
+            matrix = new char[size_x][];
+            for (int row = 0; row < size_x; row++)
             {
                 matrix[row] = Console.ReadLine().ToCharArray();
             }
+            int[] PlayerPosition = FindPlayer();
+            player_x = PlayerPosition[0];
+            player_y = PlayerPosition[1];
+            matrix[player_x][player_y] = '.';
 
             char[] commands = Console.ReadLine().ToCharArray();
 
             for (int i = 0; i < commands.Length; i++)
             {
-                int[] PlayerPosition = FindPlayer(matrix, size);
-                string result = PlayerMove(PlayerPosition[0], PlayerPosition[1], commands[i], size, matrix);                
-                List<int[]> bunnies = FindBunnies(matrix, size);
-                result += BunniesSpread(bunnies, matrix, size);
-                if (FindPlayer(matrix,size) == null)
+                PlayerMove(commands[i]);
+                BunniesSpread();
+                PrintMatrix();
+                Console.WriteLine();
+                if (matrix[player_x][player_y] == 'B')
                 {
-                    for (int row = 0; row < size[0]; row++)
-                    {
-                        for (int col = 0; col < size[1]; col++)
-                        {
-                            Console.Write(matrix[row][col]);
-                        }
-                        Console.WriteLine();
-                    }
-                    Console.WriteLine(result);
+                    PrintMatrix();
+                    Console.WriteLine($"dead: {player_x} {player_y}");
                     return;
+                }
+                if (!isCellValid(player_x, player_y))
+                {
+                    PrintMatrix();
+                    Console.WriteLine($"won: {player_x} {player_y}");
+                    break;
                 }
             }
         }
 
-        static List<int[]> FindBunnies(char[][] matrix, int[] size)
+        static void PrintMatrix()
         {
-            List<int[]> bunniesPositions = new List<int[]>();
-            for (int row = 0; row < size[0]; row++)
+            for (int row = 0; row < size_x; row++)
             {
-                for (int col = 0; col < size[1]; col++)
+                for (int col = 0; col < size_y; col++)
+                {
+                    if (player_x == row && player_y == col && matrix[player_x][player_y] != 'B')
+                    {
+                        Console.Write("P");
+                        continue;
+                    }
+                    Console.Write(matrix[row][col]);
+                }
+                Console.WriteLine();
+            }
+        }
+        
+        static void BunniesSpread()
+        {
+            char[][] matrixCopy = (char[][])matrix.Clone();
+            for (int row = 0; row < size_x; row++)
+            {
+                for (int col = 0; col < size_y; col++)
                 {
                     if (matrix[row][col] == 'B')
                     {
-                        bunniesPositions.Add(new int[2] {row, col}); 
+                        //spread left
+                        if (isCellValid(row, col - 1))
+                        {
+                            matrixCopy[row][col - 1] = 'B';
+                        }
+                        // right
+                        if (isCellValid(row, col + 1))
+                        {
+                            matrixCopy[row][col + 1] = 'B';
+                        }
+                        // up
+                        if (isCellValid(row - 1, col))
+                        {
+                            matrixCopy[row - 1][col] = 'B';
+                        }
+                        // down
+                        if (isCellValid(row + 1, col))
+                        {
+                            matrixCopy[row + 1][col] = 'B';
+                        }
                     }
                 }
             }
-            return bunniesPositions;
+            matrix = matrixCopy;
         }
 
-        static string BunniesSpread(List<int[]> bunnies, char[][] matrix, int[] size)
+        static int[] FindPlayer()
         {
-            string result = "";
-            for (int i = 0; i < bunnies.Count; i++)
+            for (int row = 0; row < size_x; row++)
             {
-                int row = bunnies[i][0];
-                int col = bunnies[i][1];
-                //spread left
-                if (isCellValid(row, col - 1, size))
-                {
-                    if (matrix[row][col - 1] == 'P')
-                    {
-                        matrix[row][col - 1] = 'B';
-                        result = $"dead: {row} {col - 1}";
-                    }
-                    matrix[row][col - 1] = 'B';
-                }
-
-                if (isCellValid(row, col + 1, size))
-                {
-                    if (matrix[row][col + 1] == 'P')
-                    {
-                        matrix[row][col + 1] = 'B';
-                        result = $"dead: {row} {col + 1}";
-                    }
-                    matrix[row][col + 1] = 'B';
-                }
-                if (isCellValid(row - 1, col, size))
-                {
-                    if (matrix[row - 1][col] == 'P')
-                    {
-                        matrix[row - 1][col] = 'B';
-                        result = $"dead: {row - 1} {col}";
-                    }
-                    matrix[row - 1][col] = 'B';
-                }
-                if (isCellValid(row + 1, col, size))
-                {
-                    if (matrix[row + 1][col] == 'P')
-                    {
-                        if (matrix[row + 1][col] == 'P')
-                        result = $"dead: {row + 1} {col}";
-                    }
-                    matrix[row + 1][col] = 'B';
-                }
-            }
-            return result;
-        }
-        static int[] FindPlayer(char[][] matrix, int[] size)
-        {
-            for (int row = 0; row < size[0]; row++)
-            {
-                for (int col = 0; col < size[1]; col++)
+                for (int col = 0; col < size_y; col++)
                 {
                     if (matrix[row][col] == 'P')
                     {
@@ -123,88 +124,29 @@ namespace _10._Radioactive_Mutant_Vampire_Bunnies
             return null;
         }
 
-        static bool isCellValid(int row, int col, int[] size)
+        static bool isCellValid(int row, int col)
         {
-            return row >= 0 && col >= 0 && row < size[0] && col < size[1];
+            return row >= 0 && col >= 0 && row < size_x && col < size_y;
         }
 
-        static string PlayerMove(int row, int col, char command, int[] size, char[][] matrix)
+        static void PlayerMove(char command)
         {
-            string result = "";
-            matrix[row][col] = '.';
             if (command == 'L')
             {
-                if (isCellValid(row, col - 1, size))
-                {
-                    if (matrix[row][col - 1] == 'B')
-                    {
-                        result = $"dead: {row} {col - 1}";
-                    }
-                    else
-                    {
-                        matrix[row][col - 1] = 'P';
-                    }
-                }
-                else
-                {
-                    result = $"won: {row} {col}";
-                }
+                player_x -= 1;
             }
-            if (command == 'R')
+            else if (command == 'R')
             {
-                if (isCellValid(row, col + 1, size))
-                {
-                    if (matrix[row][col + 1] == 'B')
-                    {
-                        result = $"dead: {row} {col + 1}";
-                    }
-                    else
-                    {
-                        matrix[row][col + 1] = 'P';
-                    }
-                }
-                else
-                {
-                    result = $"won: {row} {col}";
-                }
+                player_x += 1;
             }
-            if (command == 'U')
+            else if (command == 'U')
             {
-                if (isCellValid(row - 1, col, size))
-                {
-                    if (matrix[row - 1][col] == 'B')
-                    {
-                        result = $"dead: {row - 1} {col}";
-                    }
-                    else
-                    {
-                        matrix[row - 1][col] = 'P';
-                    }
-                }
-                else
-                {
-                    result = $"won: {row} {col}";
-                }
+                player_y -= 1;
             }
-            if (command == 'D')
+            else if (command == 'D')
             {
-                if (isCellValid(row + 1, col, size))
-                {
-                    if (matrix[row + 1][col] == 'B')
-                    {
-                        result = $"dead: {row + 1} {col}";
-                    }
-                    else
-                    {
-                        matrix[row + 1][col] = 'P';
-                    }
-                }
-                else
-                {
-                    result = $"won: {row} {col}";
-                }
+                player_y += 1;
             }
-            return result;
         }
     }
 }
