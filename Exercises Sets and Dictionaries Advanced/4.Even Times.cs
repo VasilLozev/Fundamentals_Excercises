1d1ad1c66627f972e3a4c94e3d060a84c4145087
VasilLozev <108930879+VasilLozev@users.noreply.github.com>
2022-11-21 09:54:10 +0200

    Add files via upload

---
+++ Exercises Sets and Dictionaries Advanced/4.Even Times.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _04._Even_Times
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            Dictionary<int, List<int>> set = new Dictionary<int, List<int>>();
+            for (int i = 0; i < n; i++)
+            {
+                int number = int.Parse(Console.ReadLine());
+                if (!set.ContainsKey(number))
+                {
+                     set.Add(number, new List<int>());
+                }
+                set[number].Add(1);
+                
+            }
+            foreach (var item in set)
+            {
+                if (item.Value.Sum() % 2 == 0)
+                {
+                    Console.WriteLine(item.Key);
+                }
+            }
+        }
+    }
+}
