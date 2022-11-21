f4423a375545ee4a364575ba8e086cf48c231eb4
VasilLozev <108930879+VasilLozev@users.noreply.github.com>
2022-11-21 10:05:50 +0200

    Add files via upload

---
+++ Exercises Sets and Dictionaries Advanced/5. Count Symbols.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _5._Count_Symbols
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string x = Console.ReadLine();
+            Dictionary<char, int> dict = new Dictionary<char, int>();
+
+            foreach (var item in x)
+            {
+                if (!dict.ContainsKey(item))
+                {
+                    dict.Add(item, 0);
+                }
+                dict[item]++;
+            }
+            dict = dict.OrderBy(x => x.Key).ToDictionary(x => x.Key, x => x.Value);
+            foreach (var item in dict)
+            {
+                Console.WriteLine($"{item.Key}: {item.Value} time/s");
+            }
+        }
+    }
+}
