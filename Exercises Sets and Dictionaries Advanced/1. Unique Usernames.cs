8478800dd556ab7e1dda5b01fcd9e9b02b7201b1
VasilLozev <108930879+VasilLozev@users.noreply.github.com>
2022-11-21 05:49:07 +0200

    Add files via upload

---
+++ Exercises Sets and Dictionaries Advanced/1. Unique Usernames.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _1._Unique_Usernames
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+
+            Dictionary<string, int> names = new Dictionary<string, int>();
+
+            for (int i = 0; i < n; i++)
+            {
+                string name = Console.ReadLine();
+                if (!names.ContainsKey(name))
+                {
+                    names.Add(name, i);
+                }
+            }
+            foreach (var name in names)
+            {
+                Console.WriteLine(name.Key);
+            }
+        }
+    }
+}
