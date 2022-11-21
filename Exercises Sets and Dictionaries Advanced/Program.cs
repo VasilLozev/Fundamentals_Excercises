6243225617aa8dce09a398699713089479cd9f30
VasilLozev <108930879+VasilLozev@users.noreply.github.com>
2022-11-21 09:31:40 +0200

    Add files via upload

---
+++ Exercises Sets and Dictionaries Advanced/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _3._Periodic_Table
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            HashSet<string> set = new HashSet<string>();
+            for (int i = 0; i < n; i++)
+            {
+                List<string> x = Console.ReadLine().Split(' ').ToList();
+                foreach (var item in x)
+                {
+                   if(!set.TryGetValue(item, out var value))
+                    {
+                        set.Add(item);
+                    }
+                }
+            }
+            Console.WriteLine(string.Join(' ', set.OrderBy(x=>x)));
+        }
+    }
+}
