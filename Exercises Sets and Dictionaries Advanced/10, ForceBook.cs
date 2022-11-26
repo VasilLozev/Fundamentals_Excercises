44551437103fbab17d2e56377536d23a5507d222
VasilLozev <108930879+VasilLozev@users.noreply.github.com>
2022-11-26 07:35:46 +0200

    Update 10, ForceBook.cs

--- Exercises Sets and Dictionaries Advanced/10, ForceBook.cs
+++ Exercises Sets and Dictionaries Advanced/10, ForceBook.cs
@@ -1,15 +1,14 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading;
 
 namespace _10__ForceBook
 {
-
     class Repository
     {
+        public HashSet<string> Users = new HashSet<string>();
         public Dictionary<string, HashSet<string>> forceUsers = new Dictionary<string, HashSet<string>>();
-
     }
     internal class Program
     {
@@ -26,34 +25,37 @@ namespace _10__ForceBook
                 if (input.Contains("|"))
                 {
                     string[] input1 = input.Split(" | ");
-                    if (!repository.forceUsers.ContainsKey(input1[0]))
+                    if (!repository.Users.Contains(input1[1]))
                     {
-                        repository.forceUsers.Add(input1[0], new HashSet<string>());
-                    }
-                    if (!repository.forceUsers[input1[0]].Contains(input1[1]))
-                    {
+                        repository.Users.Add(input1[1]);
+                        if (!repository.forceUsers.ContainsKey(input1[0]))
+                        {
+                            repository.forceUsers.Add(input1[0], new HashSet<string>());
+                        }
                         repository.forceUsers[input1[0]].Add(input1[1]);
                     }
                 }
                 else
                 {
                     string[] input1 = input.Split(" -> ");
+                    if (!repository.forceUsers.ContainsKey(input1[1]))
+                    {
+                        repository.forceUsers.Add(input1[1], new HashSet<string>());
+                    }
                     foreach (var key in repository.forceUsers.Keys)
                     {
                         if (repository.forceUsers[key].Contains(input1[0]))
                         {
                             repository.forceUsers[key].Remove(input1[0]);
                         }
-                        if (!repository.forceUsers[input1[1]].Contains(input1[0]))
-                        {
-                            repository.forceUsers[input1[1]].Add(input1[0]);
-                        }
                     }
-
+                    repository.forceUsers[input1[1]].Add(input1[0]);
+                    repository.Users.Add(input1[0]);
                     Console.WriteLine($"{input1[0]} joins the {input1[1]} side!");
                 }
             }
-            repository.forceUsers = repository.forceUsers.OrderByDescending(x => x.Value.Count)
+           
+                repository.forceUsers = repository.forceUsers.OrderByDescending(x => x.Value.Count)
                 .ThenBy(x => x.Key)
                 .ToDictionary(x => x.Key, x => x.Value);
 
@@ -62,9 +64,9 @@ namespace _10__ForceBook
                 if (repository.forceUsers[forceSide].Count != 0)
                 {
                     Console.WriteLine($"Side: {forceSide}, Members: {repository.forceUsers[forceSide].Count}");
-                    HashSet<string> forceUsers = repository.forceUsers[forceSide]
+                    List<string> forceUsers = repository.forceUsers[forceSide]
                         .OrderBy(x => x)
-                        .ToHashSet();
+                        .ToList();
                     foreach (var forceUser in forceUsers)
                     {
                         Console.WriteLine($"! {forceUser}");
