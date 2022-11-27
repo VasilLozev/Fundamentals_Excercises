6f568ed228ca16eb0dcf3de899aa9b3a8fe673de
VasilLozev <108930879+VasilLozev@users.noreply.github.com>
2022-11-27 14:13:59 +0200

    Add files via upload

---
+++ Streams, Files and Directories/1.File Operations/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OddLines
+{
+    
+    public class OddLines
+    {
+        static void Main(string[] args)
+        {
+            string inputFilePath = @"..\..\..\input.txt";
+            string outputFilePath = @"..\..\..\output.txt";
+
+            ExtractOddLines(inputFilePath, outputFilePath);
+        }
+
+        public static void ExtractOddLines(string inputFilePath, string outputFilePath)
+        {
+            using (StreamReader reader = new StreamReader(inputFilePath))
+            {
+                using (StreamWriter writer = new StreamWriter(outputFilePath))
+                {
+                    int row = 0;
+                    while (!reader.EndOfStream)
+                    {
+                        string line = reader.ReadLine();
+                        if (row++ % 2 == 1)
+                        {
+                            writer.WriteLine(line);
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ Streams, Files and Directories/3.Word Count/Program.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+
+namespace WordCount
+{
+    public class WordCount
+    {
+        static void Main()
+        {
+            string wordPath = @"..\..\..\Files\words.txt";
+            string textPath = @"..\..\..\Files\text.txt";
+            string outputPath = @"..\..\..\Files\output.txt";
+
+            CalculateWordCounts(wordPath, textPath, outputPath);
+        }
+
+        public static void CalculateWordCounts(string wordsFilePath, string textFilePath, string outputFilePath)
+        {
+            using (StreamReader reader = new StreamReader(wordsFilePath))
+            {
+                    using (StreamReader reader1 = new StreamReader(textFilePath))
+                    {
+                        using (StreamWriter writer = new StreamWriter(outputFilePath))
+                        {
+                            while (!reader.EndOfStream)
+                            {
+                                string[] read = reader1.ReadToEnd().Split(' ', '-', ',', '.');
+                                
+                            while (!reader.EndOfStream)
+                            {
+                                string[] words = reader.ReadToEnd().Split(' ');
+                                Dictionary<string, int> dict = new Dictionary<string, int>();
+                                for (int i = 0; i < words.Length; i++)
+                                {
+                                    int count = 0;
+                                    foreach (var item in read)
+                                    {
+                                        if (words[i].Equals( item,
+                   StringComparison.OrdinalIgnoreCase))
+                                        {
+                                            count++;
+                                        }
+                                    }
+                                    dict.Add(words[i],count);
+                                }
+                                dict = dict.OrderByDescending(x => x.Value).ToDictionary(x => x.Key, x => x.Value);
+                                foreach (var key in dict.Keys)
+                                {
+                                    writer.WriteLine($"{key} - {dict[key]}");
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
