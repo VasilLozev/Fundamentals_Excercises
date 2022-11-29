6e37ab0e2263c26e9efc0d24669ff0542d4af623
VasilLozev <108930879+VasilLozev@users.noreply.github.com>
2022-11-29 11:01:10 +0200

    Add files via upload

---
+++ Streams, Files and Directories_Exercise/DirectoryTraversal.cs
@@ -0,0 +1,64 @@
+﻿namespace DirectoryTraversal
+{
+    using System;
+    using System.Collections.Generic;
+    using System.IO;
+    using System.Linq;
+    using System.Text;
+
+    public class DirectoryTraversal
+    {
+        static void Main()
+        {
+            string path = Console.ReadLine();
+            string reportFileName = @"\report.txt";
+
+            string reportContent = TraverseDirectory(path);
+            Console.WriteLine(reportContent);
+
+            WriteReportToDesktop(reportContent, reportFileName);
+        }
+
+        public static string TraverseDirectory(string inputFolderPath)
+        {
+            SortedDictionary<string, List<FileInfo>> extensionsFiles = new SortedDictionary<string, List<FileInfo>>();
+
+            string[] files = Directory.GetFiles(inputFolderPath);
+
+            foreach (string file in files)
+            {
+                FileInfo fileinfo = new FileInfo(file);
+                if (!extensionsFiles.ContainsKey(fileinfo.Extension))
+                {
+                    extensionsFiles.Add(fileinfo.Extension, new List<FileInfo>());
+                }
+                extensionsFiles[fileinfo.Extension].Add(fileinfo);
+            }
+
+            var orderedExtensionsFiles = extensionsFiles.OrderByDescending(ef => ef.Value.Count);
+
+            StringBuilder sb = new StringBuilder();
+
+            foreach (var extensionFiles in orderedExtensionsFiles)
+            {
+                sb.AppendLine(extensionFiles.Key);
+
+                var orderedFiles = extensionFiles.Value.OrderByDescending(f => f.Length);
+
+                foreach (var file in orderedFiles)
+                {
+                    sb.AppendLine($"--{file.Name} - {(double)file.Length / 1024:f3}KB");
+                }
+            }
+
+            return sb.ToString();
+        }
+
+        public static void WriteReportToDesktop(string textContent, string reportFileName)
+        {
+            string filePath = Environment.GetFolderPath(Environment.SpecialFolder.Desktop) + reportFileName;
+
+            File.WriteAllText(filePath, textContent);
+        }
+    }
+}
---
+++ Streams, Files and Directories_Exercise/LineNumbers.cs
@@ -0,0 +1,38 @@
+﻿namespace LineNumbers
+{
+    using System;
+    using System.IO;
+    using System.Linq;
+    using System.Text;
+    using System.Threading;
+    using System.Xml;
+
+    public class LineNumbers
+    {
+        static void Main()
+        {
+            string inputFilePath = @"..\..\..\text.txt";
+            string outputFilePath = @"..\..\..\output.txt";
+
+            ProcessLines(inputFilePath, outputFilePath);
+        }
+
+        public static void ProcessLines(string inputFilePath, string outputFilePath)
+        {
+            StringBuilder sb = new StringBuilder();
+
+            string[] lines = File.ReadAllLines(inputFilePath);
+            
+                for (int i = 0; i < lines.Length; i++)
+                {
+                    int letterCount = lines[i].Count(ch => char.IsLetter(ch));
+                    int symbolCount = lines[i].Count(ch => char.IsPunctuation(ch));
+
+                    sb.AppendLine($"Line {i + 1}: -I was quick to judge him, but it wasn't his fault. ({letterCount})({symbolCount})");
+                }
+
+                File.WriteAllText(outputFilePath, sb.ToString());
+            
+        }
+    }
+}
