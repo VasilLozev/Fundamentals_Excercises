a448f77ea4f770714048c8a5685dbea1930b3ee0
VasilLozev <108930879+VasilLozev@users.noreply.github.com>
2022-12-09 20:10:10 +0200

    Add files via upload

---
+++ Node-LinkedList/SoftUniLinkedList.cs
@@ -0,0 +1,101 @@
+﻿using SoftUniLinkedList;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Transactions;
+
+namespace SoftUniLinkedList
+{
+    public class LinkedList
+    {
+        public Node Head { get; set; }
+        public Node Tail { get; set; }
+        public int Count { get; set; }
+
+        public void AddFirst(Node node)
+        {
+            Count++;
+            if (Tail == null)
+            {
+                Tail = node;
+                Head = node;
+                return;
+            }
+            Head.Previous = node;
+            node.Next = Head;
+            Head = node;
+        }
+        
+        public void AddFirst(int value)
+        {
+
+            AddFirst(new Node(value));
+        }
+        public void AddLast(Node node)
+        {
+            Count++;
+            if (Head == null)
+            {
+                Tail = node;
+                Head = node;
+                return;
+            }
+            Tail.Next = node;
+            node.Previous = Tail;
+            Tail = node;
+        }
+        public void AddLast(int value)
+        {
+            AddLast(new Node(value));
+        }
+       
+        public int RemoveFirst()
+        {
+            Count--;
+            Node oldHead = Head;
+            Head = Head.Next;
+            Head.Previous = null;
+            oldHead.Next = null;
+            return oldHead.Value;
+        }
+        public int RemoveLast()
+        {
+            Count--;
+            Node oldTail = Tail;
+            Tail = Tail.Previous;
+            Tail.Next = null;
+            Tail.Previous = null;
+            return oldTail.Value;
+        }
+
+        public void ForEach(Action<int> callback)
+        {
+            Node node = Head;
+            while (node != null)
+            {
+                callback(node.Value);
+                node = node.Next;
+            }
+        }
+        public void ForEachReverse(Action<int> callback)
+        {
+            Node node = Tail;
+            while (node != null)
+            {
+                callback(node.Value);
+                node = node.Previous;
+            }
+        }
+
+        public int[] ToArray()
+        {
+            int[] array = new int[Count];
+            int index = 0;
+            ForEach(n =>
+            {
+                array[index++] = n;
+            });
+            return array;
+        }
+    }
+}
---
+++ Node-LinkedList/StartUp.cs
@@ -0,0 +1,67 @@
+﻿using SoftUniLinkedList;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Xml.Linq;
+
+namespace SoftUniLinkedList
+{
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+
+            LinkedList list = new LinkedList();
+
+            list.AddFirst(1);
+            list.AddFirst(2);
+            list.AddLast(3);
+            list.AddFirst(4);
+
+            //  list.RemoveFirst();
+            // list.RemoveLast();
+
+            list.ForEach(number =>
+            {
+                Console.WriteLine($"Each number in list:{number}");
+            });
+            Console.WriteLine("Reverse");
+            list.ForEachReverse(number =>
+            {
+                Console.WriteLine($"Each number in list:{number}");
+            });
+
+            Console.WriteLine(String.Join(',', list.ToArray()));
+
+            /*StackWithLinkedList stack   =  new StackWithLinkedList();
+
+            stack.Add(1);
+            stack.Add(2);
+
+            Console.WriteLine(stack.Pop());
+            Console.WriteLine(stack.Pop());
+            return;
+
+           SoftUniLinkedList LinkedList = new SoftUniLinkedList();
+
+            LinkedList.AddFirst(new Node(1));
+            LinkedList.AddFirst(new Node(2));
+            LinkedList.AddFirst(new Node(3));
+            LinkedList.AddFirst(new Node(4));*/
+
+            /*  while (LinkedList.Count >= 0)
+              {
+                  Console.WriteLine(LinkedList.RemoveFirst());
+              }
+  */
+            /* int currentNode = softUniLinkedList.RemoveFirst();
+
+             while (currentNode != null)
+             {
+                 Console.WriteLine(currentNode.value);
+                 currentNode = currentNode.Next;
+             } */
+        }
+    }
+}
