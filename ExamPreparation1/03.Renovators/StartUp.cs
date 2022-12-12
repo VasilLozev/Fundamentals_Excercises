0094763d5ccb20384b1dd04757d7910339876ce7
VasilLozev <108930879+VasilLozev@users.noreply.github.com>
2022-12-12 15:37:53 +0200

    Add files via upload

---
+++ ExamPreparation1/03.Renovators/StartUp.cs
@@ -0,0 +1,131 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Renovators
+{
+    public class StartUp
+    {
+        public static void Main()
+        {
+            int size = int.Parse(Console.ReadLine());
+            char[][] matrix = new char[size][];
+            int Row = 0;
+            int Col = 0;
+            int countOfHoles = 1;
+            int countOfRods = 0;
+            string command = "";
+            for (int i = 0; i < size; i++)
+            {
+                matrix[i] = Console.ReadLine().ToCharArray();
+                for (int j = 0; j < size; j++)
+                {
+                    if (matrix[i][j] == 'V')
+                    {
+                        Row = i;
+                        Col = j;
+                        matrix[i][j] = '*';
+
+                    }
+                }
+            }
+            
+            while (true)
+            {
+                command = Console.ReadLine();
+                if (command == "End")
+                {
+                    break;
+                }               
+                var item = Move(size, countOfRods, countOfHoles, command, matrix, Row, Col);
+                matrix = item.Item1;
+                Row = item.row;
+                Col = item.col;
+                countOfHoles = item.countOfHoles;
+                countOfRods = item.countOfRods;
+                foreach (var rows in matrix)
+                {
+                    if (rows.Contains('E'))
+                    {
+                        Console.WriteLine($"Vanko got electrocuted, but he managed to make {countOfHoles} hole(s).");
+                        foreach (var items in matrix)
+                        {
+                            Console.WriteLine(items);
+                        }
+                        return;
+                    }
+                }
+            }
+            if (command == "End")
+            {
+                Console.WriteLine($"Vanko managed to make {countOfHoles} hole(s) and he hit only {countOfRods} rod(s).");
+                matrix[Row][Col] = 'V';
+                foreach (var rows in matrix)
+                {
+                    Console.WriteLine(rows);
+                }
+                return;
+            }
+        }
+        public static (char[][],int countOfRods, int countOfHoles,int row, int col) Move(int size, int countOfRods, int countOfHoles, string command, char[][] matrix, int row, int col)
+        {
+            int Row = row;
+            int Col = col;          
+            switch (command)
+            {
+                case ("up"):
+                    row--;
+                    break;
+                case ("down"):
+                    row++;
+                    break;
+                case ("left"):
+                    col--;
+                    break;
+                case ("right"):
+                    col++;
+                    break;
+            }
+            if (row >= size || row < 0)
+            {
+                row = Row;
+                col = Col;
+                return (matrix, countOfRods, countOfHoles, row, col);
+            }
+            if (col >= size || col < 0)
+            {
+                row = Row;
+                col = Col;
+                return (matrix, countOfRods, countOfHoles, row, col);
+            }
+            if (matrix[row][col] == 'R')
+            {
+                row = Row;
+                col = Col;
+                countOfRods++;
+                
+                Console.WriteLine("Vanko hit a rod!");
+                return (matrix, countOfRods, countOfHoles, row, col);
+            }
+            if (matrix[row][col] == 'C')
+            {
+                matrix[row][col] = 'E';
+                countOfHoles++;
+                return (matrix, countOfRods, countOfHoles, row, col);
+            }
+            if (matrix[row][col] == '*')
+            {
+                Console.WriteLine($"The wall is already destroyed at position [{row}, {col}]!");
+                return (matrix, countOfRods, countOfHoles, row, col);
+            }
+            else
+            {
+               matrix[row][col] = '*';
+               countOfHoles++;
+            }
+            return (matrix, countOfRods, countOfHoles, row, col);
+        }
+    }
+}
---
+++ ExamPreparation1/1. Tiles Master/StartUp.cs
@@ -0,0 +1,122 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Renovators
+{
+    public class StartUp
+    {
+        static void Main()
+        {
+            Stack<int> whiteTiles = new Stack<int>(Console.ReadLine().Split(' ').Select(int.Parse));
+            Queue<int> GreyTiles = new Queue<int>(Console.ReadLine().Split(' ').Select(int.Parse));
+            Dictionary<string, int> LocationCatalog = new Dictionary<string, int>();
+                
+            while (true)
+            {
+                if (whiteTiles.Peek() == GreyTiles.Peek())
+                {
+                    int sum = whiteTiles.Peek() + GreyTiles.Peek();
+                    switch (sum)
+                    {
+                        case (40):
+                            if (!LocationCatalog.ContainsKey("Sink"))
+                            {
+                                LocationCatalog.Add("Sink", 0);
+                            }
+                            LocationCatalog["Sink"]++;
+                            whiteTiles.Pop();
+                            GreyTiles.Dequeue();
+                            break;
+                        case (50):
+                            if (!LocationCatalog.ContainsKey("Oven"))
+                            {
+                                LocationCatalog.Add("Oven", 0);
+                            }
+                            LocationCatalog["Oven"]++;
+                            whiteTiles.Pop();
+                            GreyTiles.Dequeue();
+                            break;
+                        case (60):
+                            if (!LocationCatalog.ContainsKey("Countertop"))
+                            {
+                                LocationCatalog.Add("Countertop", 0);
+                            }
+                            LocationCatalog["Countertop"]++;
+                            whiteTiles.Pop();
+                            GreyTiles.Dequeue();
+                            break;
+                        case (70):
+                            if (!LocationCatalog.ContainsKey("Wall"))
+                            {
+                                LocationCatalog.Add("Wall", 0);
+                            }
+                            LocationCatalog["Wall"]++;
+                            whiteTiles.Pop();
+                            GreyTiles.Dequeue();
+                            break;
+                        default:
+                            if (!LocationCatalog.ContainsKey("Floor"))
+                            {
+                                LocationCatalog.Add("Floor", 0);
+                            }
+                            LocationCatalog["Floor"]++;
+                            whiteTiles.Pop();
+                            GreyTiles.Dequeue();
+                            break;
+                    }
+                }
+                else
+                {
+                    int halfWhite = whiteTiles.Pop();
+                    halfWhite /= 2;
+                    whiteTiles.Push(halfWhite);
+                    int grey = GreyTiles.Dequeue();
+                    GreyTiles.Enqueue(grey);
+                }
+                if (whiteTiles.Count == 0 && GreyTiles.Count == 0)
+                {
+                    Console.WriteLine("White tiles left: none");
+                    Console.WriteLine("Grey tiles left: none");
+                    break;
+                }
+                else if (whiteTiles.Count == 0 || GreyTiles.Count == 0)
+                {
+                    if (whiteTiles.Count > 0)
+                    {
+                        StringBuilder sb = new StringBuilder();
+                        sb.Append("White tiles left: ");
+                        sb.Append(String.Join(", ", whiteTiles));
+                        Console.WriteLine(sb.ToString());
+                    }
+                    else
+                    {
+                        Console.WriteLine("White tiles left: none");
+                    }
+                    if (GreyTiles.Count > 0)
+                    {
+                        StringBuilder sb = new StringBuilder();
+                        sb.Append("Grey tiles left: ");
+                        sb.Append(String.Join(", ", GreyTiles));
+                        Console.WriteLine(sb.ToString());
+                    }
+                    else
+                    {
+                        Console.WriteLine("Grey tiles left: none");
+                    }
+                    break;
+                }
+            }
+
+
+            LocationCatalog = LocationCatalog.OrderByDescending(x => x.Value).ThenBy(x => x.Key)
+                .ToDictionary(x=>x.Key, x=>x.Value);
+            foreach (var item in LocationCatalog.Keys)
+            {
+                Console.WriteLine($"{item}: {LocationCatalog[item]}");
+            }
+        }
+    }
+}
